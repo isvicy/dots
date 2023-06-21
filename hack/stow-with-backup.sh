f790d6d16d4a33cbb6f859a0262f4ea598ef28ca
ak2kay <isregistermail@gmail.com>
2023-06-21 07:08:45 +0000

    feat: create parent dir before using stow to link dots files
    In this way, git won't treat files in sub dirs of dir such as ~/.config as
    untracked file.

--- hack/stow-with-backup.sh
+++ hack/stow-with-backup.sh
@@ -3,11 +3,21 @@
 package=$1
 target=$2
 
+script=$(readlink -f "$0")
+base_dir=$(dirname "$script")
+
+. "${base_dir}"/utils.sh
+
+ensureTargetDir "${HOME}/.config.bak"
+ensureTargetDir "${HOME}/.config"
+ensureTargetDir "${HOME}/.local"
+ensureTargetDir "${HOME}/.local/bin"
+ensureTargetDir "${HOME}/.local/share"
+
 if ! stow --target="$target" "$package" --restow; then
 	# find the conflicting files
 	conflicts=$(stow --simulate --verbose=2 --target="$target" "$package" 2>&1 | grep "existing target is")
 	bakdir="${HOME}"/.config.bak
-	mkdir "${bakdir}"
 
 	# loop over each conflicting file
 	while read -r conflict; do
---
+++ hack/utils.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+asksure() {
+	while read -r -n 1 -s answer; do
+		if [[ $answer = [YyNn] ]]; then
+			[[ $answer = [Yy] ]] && retval=0
+			[[ $answer = [Nn] ]] && retval=1
+			break
+		fi
+	done
+
+	echo # just a final linefeed, optics...
+
+	return "$retval"
+}
+
+ensureTargetDir() {
+	dir=${1}
+	if [[ ! -d "${dir}" ]]; then
+		mkdir -p "${dir}"
+	fi
+}
