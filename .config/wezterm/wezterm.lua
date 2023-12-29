debbc6fd43376266e6b8cf2a97e1d867c52cafd4
ak2kay <yandli@hotmail.com>
2023-12-29 22:30:06 +0800

    feat(wezterm): add tab rename hot-key

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -37,6 +37,21 @@ config.keys = {
 		action = act.SpawnTab("CurrentPaneDomain"),
 	},
 	{ key = "l", mods = "LEADER", action = act.ShowLauncher },
+	{
+		key = "e",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 for i = 1, 8 do
 	-- <leader> + number to activate that tab
