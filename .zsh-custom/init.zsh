f34189075e14467ddac781ff65173f7bf5298bdc
isvicy <isregistermail@gmail.com>
2024-06-25 03:30:02 +0000

    fix(direnv): fix missing nix when create new pane in tmux

---
+++ .zsh-custom/init.zsh
@@ -0,0 +1,6 @@
+# Nix shoulbe be loaded before direnv
+# Nix
+if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+fi
+# End Nix
--- .zsh-custom/thirdparty.zsh
+++ .zsh-custom/thirdparty.zsh
@@ -1,9 +1,3 @@
 if command -v zoxide >/dev/null 2>&1; then
   eval "$(zoxide init zsh)"
 fi
-
-# Nix
-if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-  . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-fi
-# End Nix
