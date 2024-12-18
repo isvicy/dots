d79bfa591185d312e4eccd7c82c390dcb6270fad
isvicy <isregistermail@gmail.com>
2024-12-18 14:16:44 +0800

    change: use nixos to manage locale

--- .zsh-custom/env.zsh
+++ .zsh-custom/env.zsh
@@ -1,15 +1,9 @@
 export PAGER=less                              # Use 'less' as the default pager for viewing long outputs
 export EDITOR='nvim'                           # Set Neovim as the default text editor
 export GPG_TTY=$TTY                            # Set GPG to use the current terminal for passphrase prompts
-unset LC_ALL
-export LANGUAGE=en_US.UTF-8                    # Set the language for the locale to English (UTF-8)
-export LANG=en_US.UTF-8                        # Set the default locale to English (UTF-8)
-export LC_CTYPE=en_US.UTF-8                    # Set the character encoding to UTF-8 for the terminal
 export SYSTEMD_LESS=${LESS}S                   # Configure 'less' for systemd logs to fold long lines
 export MANOPT=--no-hyphenation                 # Display man pages without hyphenation for better readability
 export XDG_CONFIG_HOME="$HOME/.config"         # Set the base directory for user-specific configuration files
-# fix locale warning of nvim which installed by nix
-export LOCALE_ARCHIVE=/usr/lib/locale/locale-archive
 
 export PATH="${HOME}/.local/bin":${PATH}       # add local path for current user
 
