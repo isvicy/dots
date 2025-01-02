3db8d02b87126004f0007f15d7d4dea6c8746d2a
aaron <isregistermail@gmail.com>
2025-01-03 00:54:19 +0800

    fix: make sure the binary from nix can be found by macos

--- .zsh-custom/init.zsh
+++ .zsh-custom/init.zsh
@@ -2,5 +2,6 @@
 # Nix
 if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
   . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  export PATH=/etc/profiles/per-user/${USER}/bin:/run/current-system/sw/bin:${PATH}
 fi
 # End Nix
