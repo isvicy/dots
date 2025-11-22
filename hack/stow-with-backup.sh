38b718f8c67f50144902fc165deb963f6d92c57b
isvicy <isregistermail@gmail.com>
2025-11-22 22:27:27 +0800

    feat: add default mcp config

--- hack/stow-with-backup.sh
+++ hack/stow-with-backup.sh
@@ -15,6 +15,7 @@ ensureTargetDir "${HOME}/.config"
 ensureTargetDir "${HOME}/.local"
 ensureTargetDir "${HOME}/.local/bin"
 ensureTargetDir "${HOME}/.local/share"
+ensureTargetDir "${HOME}/.mcp"
 
 # Run stow in simulation mode to detect conflicts
 # use uniq to remove duplicates cause stow will output the confilcts info multiple times
