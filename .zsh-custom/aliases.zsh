ed5d2673705c98e128cde0240016944f6ea6b682
isvicy <isregistermail@gmail.com>
2025-11-23 19:57:32 +0800

    feat: add alias for auto cd in yazi

--- .zsh-custom/aliases.zsh
+++ .zsh-custom/aliases.zsh
@@ -377,3 +377,12 @@ if command -v ikimi >/dev/null; then
         ln -s $(which ikimi) $(dirname $(which ikimi))/kimi
     fi
 fi
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
